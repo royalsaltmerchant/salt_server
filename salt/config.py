f0e2c59de408bc8fda2577d8bf689e374f120f0b
Julian ranieri <jumpingafterrain@gmail.com>
2021-06-25 12:29:50 -0700

    new db and migrations

--- salt/config.py
+++ salt/config.py
@@ -2,7 +2,12 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///test2.db'
+    # SQLALCHEMY_DATABASE_URI = 'sqlite:///test2.db'
+    # SQLALCHEMY_DATABASE_URI = (
+    #     f'postgresql+psycopg2://{os.environ.get("AUDIOGUILD_POSTGRES_USER")}:' +
+    #     f'{os.environ.get("AUDIOGUILD_POSTGRES_PW")}@{os.environ.get("AUDIOGUILD_DATABASE_URL")}/{os.environ.get("AUDIOGUILD_DATABASE_DB")}'
+    # )
+    SQLALCHEMY_DATABASE_URI = f'postgresql+psycopg2://{os.environ.get("AUDIOGUILD_POSTGRES_USER")}:{os.environ.get("AUDIOGUILD_POSTGRES_PW")}@{os.environ.get("AUDIOGUILD_DATABASE_URL")}/{os.environ.get("AUDIOGUILD_DATABASE_DB")}'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
