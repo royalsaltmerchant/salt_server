059951fc6e0120f1beee7478e63977c8f280419e
Julian ranieri <jumpingafterrain@gmail.com>
2021-11-07 10:32:47 -0800

    serialize active

--- salt/serializers.py
+++ salt/serializers.py
@@ -5,7 +5,7 @@ import logging, json
 class UserSchema(ma.SQLAlchemySchema):
     class Meta:
         model = User
-        fields = ("id", "first_name", "last_name", "username", "admin", "email", "password", "approved_asset_count", "coins", "contributions")
+        fields = ("id", "active", "first_name", "last_name", "username", "admin", "email", "password", "approved_asset_count", "coins", "contributions")
     contributions = ma.Nested('ContributionSchema', many=True)
 
 class ProjectSchema(ma.SQLAlchemySchema):
