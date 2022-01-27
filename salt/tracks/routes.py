57e536f8223f0c294b29311c626567a27ad5065c
Julian ranieri <jumpingafterrain@gmail.com>
2022-01-26 18:50:31 -0800

    move track assets array

--- salt/tracks/routes.py
+++ salt/tracks/routes.py
@@ -26,8 +26,8 @@ def api_get_paginated_track_assets_by_username(username):
 
     track_assets_by_username = db.session.query(TrackAsset).filter_by(author_username=username).all()
 
+    track_assets_to_serialize = []
     if query:
-      track_assets_to_serialize = []
       for asset in track_assets_by_username:
           metadata = asset.audio_metadata
           for item in metadata:
@@ -41,7 +41,7 @@ def api_get_paginated_track_assets_by_username(username):
               track_assets_to_serialize.append(asset)
     else:
       track_assets_by_username_paginated = db.session.query(TrackAsset).filter_by(author_username=username).order_by(asc(TrackAsset.name)).offset(offset).limit(limit)
-      track_assets_to_serialize = track_assets_by_username_paginated
+      track_assets_to_serialize.append(track_assets_by_username_paginated)
     
     track_assets_count = len(track_assets_to_serialize)
     remaining_amount = track_assets_count - (offset + limit)
@@ -72,8 +72,8 @@ def api_get_track_assets():
     else:
          query = None
 
+    track_assets_to_serialize = []
     if query:
-      track_assets_to_serialize = []
       all_track_assets = TrackAsset.query.all()
       for asset in all_track_assets:
           metadata = asset.audio_metadata
@@ -88,7 +88,7 @@ def api_get_track_assets():
               track_assets_to_serialize.append(asset)
     else:
       track_assets_paginated = TrackAsset.query.order_by(asc(TrackAsset.name)).offset(offset).limit(limit)
-      track_assets_to_serialize = track_assets_paginated
+      track_assets_to_serialize.append(track_assets_paginated)
     
     track_assets_count = len(track_assets_to_serialize)
     remaining_amount = track_assets_count - (offset + limit)
