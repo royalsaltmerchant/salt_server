1626c8273154168ff59d35e3a54c7675e2ae2852
Julian ranieri <jumpingafterrain@gmail.com>
2022-02-21 21:26:33 -0800

    change main routes, presigned doesn't need token

--- salt/main/routes.py
+++ salt/main/routes.py
@@ -16,29 +16,8 @@ main = Blueprint('main', __name__)
 track_asset_schema = TrackAssetSchema()
 track_assets_schema = TrackAssetSchema(many=True)
 
-@main.route('/', methods=['GET'])
-def health_check():
-    # TODO: check health of system with a few things here
-    # datbase connection is still solid, etc
-    result = {
-        "healthy": True,
-        "environment": os.environ.get('FLASK_ENV'),
-        "version": "0.0.1"
-    }
-    response = Response(
-        response=json.dumps(result),
-        status=200,
-        mimetype='application/json'
-    )
-    return response
-
-@main.route('/home')
-def home():
-  return render_template('home.html')
-
 @main.route('/api/signed_URL', methods=['POST'])
-@token_required
-def create_presigned_url(current_user):
+def create_presigned_url():
     s3_client = boto3.client('s3')
     try:
         data = json.loads(request.data)
